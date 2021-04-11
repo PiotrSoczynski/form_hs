3ff7460278a04dc96434eba9c76cb65a0191dfec
PiotrSoczynski <60900665+PiotrSoczynski@users.noreply.github.com>
2021-04-11 23:01:40 +0200

    Update and rename hs_form.html to hs_form.js

---
+++ hs_form.js
@@ -0,0 +1,10 @@
+<!-- [if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="https://js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif] -->
+
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script><script>hbspt.forms.create( {
+    portalId: "4713259", css:"", target:".Belch-mOaWTfyCRKbm4UngrFjl", formId: "22675ddb-363a-4362-8134-f031513fa96a", css: "", onFormReady: function($form) {
+    $('input[name="landing_name"]').val(window.location.href).change();
+  }
+});
+  </script><div class="embed-form-super-wrapper Belch-mOaWTfyCRKbm4UngrFjl"></div>
